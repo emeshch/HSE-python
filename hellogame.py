7e701436dc92231f434418567a713f00ee48c3bc
Evgenia Meshcheryakova <e-meshch@ya.ru>
2014-10-05 23:53:19 +0400

    upd

--- hellogame.py
+++ hellogame.py
@@ -21,30 +21,26 @@ BLUE = (0, 0, 255)
 
 pos = 100
 y = screen_height *3/4
-#tank = pygame.Rect((pos, y-40, 60, 40))
-#a0 = tank.centerx
-#b0 = tank.top
-l = 50 #name it!
+l = 50
 phi = math.pi/9
-#a1 = tank.centerx + l*math.cos(phi)
-#b1 = tank.top - l*math.sin(phi)
 keys = [False, False, False]
-speed = [2, 2]
 
-class Particle:
+class Particle():
     def __init__(self, (x, y), size):
         self.x = x
         self.y = y
         self.size = size
-        self.colour = (0, 0, 255)
-        self.thickness = 1
     def display(self):
-        pygame.draw.circle(screen, self.colour, (self.x, self.y), self.size, self.thickness)
+        pygame.draw.circle(screen, BLACK, (self.x, self.y), self.size)
     def move(self):
-        self.x += 20
+        self.x += 10
+        
+    def remove(self):
+        pygame.draw.circle(screen, WHITE, (self.x, self.y), self.size)
 
 done = False
-playtime=0
+playtime = 0
+#bx, by = 10, 10
 while not done:
     milliseconds = clock.tick(FPS) # do not go faster than this framerate
     playtime += milliseconds / 1000.0
@@ -52,47 +48,41 @@ while not done:
         if event.type == QUIT:
             done = True
             print('bye-bye')
-            pygame.quit()
-            sys.exit()
+            pygame.quit() #delete
+            sys.exit() #delete
         elif event.type == pygame.KEYDOWN:
             if event.key == K_d:
                 keys[0] = True
-                #print(keys[0])
                 pos += 10
                 print("step")
             elif event.key == K_w:
                 keys[1] = True
-                #print(keys[1])
                 phi += math.pi/12
-                print("up")
+                print("head up")
             elif event.key == K_s:
                 keys[2] = True
-                bul = Particle((bx, by), 4)
-                bul.display
-                bul.move
-                print("fire")
-                           
-    tank = pygame.Rect((pos, y-30, 80, 30))
+                print('fire!')                           
+    screen.fill(WHITE)    
+    pygame.draw.line(screen, BLACK, (0, y), (screen_width, y), 4)
+    tank = pygame.Rect(pos, y-30, 80, 30)
+    pygame.draw.rect(screen, RED, tank)
     a0 = tank.centerx
     b0 = tank.top
     a1 = tank.centerx + l*math.cos(phi)
     b1 = tank.top - l*math.sin(phi)
     bx, by = int(a1), int(b1)
-    
-
-    screen.fill(WHITE)
-    pygame.draw.line(screen, BLACK, (0, y), (screen_width, y), 4)
-    pygame.draw.rect(screen, RED, tank)
     pygame.draw.line(screen, RED, (a0,b0), (a1,b1), 10)
-
     
-    #pygame.draw.circle(screen, BLUE, (bx,by), 5, 0)
-        
-    pygame.display.flip()
+    if keys[2] == True:
+        bul = Particle((bx,by), 5)
+        while bul.x < screen_width and bul.y < y:
+            bul.move()
+            bul.display()
+           # time.sleep(0.02)
+            
+            
+    pygame.display.flip()    
     clock.tick(10)
-    
-    
-
 
 #pygame.display.update()
 
