902566b8d8601bb6a6e500e04b334f1eb5cd7061
Evgenia Meshcheryakova <e-meshch@ya.ru>
2014-11-10 00:14:52 +0300

    inner sorting :)

--- hw7/radix-sort.py
+++ hw7/radix-sort.py
@@ -1,5 +1,6 @@
 import random
 
+
 rand_list = random.sample(range(100), 6)
 print("l=", rand_list)
 
@@ -7,31 +8,47 @@ print("l=", rand_list)
 
 #n=10
 modlist = []
+divlist = []
 modlist = [i % 10 for i in rand_list]
-print("mod= ", modlist)
+divlist = [i // 10 for i in rand_list]
+print("div= ", divlist, "mod= ", modlist)
 
 
-def counting_sort(modlist):
-    temp = [0] * 10         # we could take not 10 but max(modlist)+1
-    print("index", [i for i in range(10)])
+
+
+def counting_sort(divlist, modlist):
+    sortedlist = []
+    temp = [0] * 10       # we could take not 10 but max(modlist)+1
+    # print("index", [i for i in range(10)])
     for item in modlist:
         # print(item)
         temp[item] += 1
-    print("temp ", temp)     # temp[i] now contains the number of elements equal to i
-    print("index", [i for i in range(10)])
+    # print("temp ", temp)     # temp[i] now contains the number of elements equal to i
+    # print("index", [i for i in range(10)])
 
     for i in range(1, len(temp)):
         temp[i] += temp[i - 1]
-    print("counted", temp)    # counted[i] now contains the number of elements <= i
+    # print("index", [i for i in range(10)])
+    # print("count", temp)    # counted[i] now contains the number of elements <= i
 
     result = [0]*(len(modlist) + 1)
-    for num in modlist:
+    div_res = [0]*(len(modlist) + 1)    #2DO rename
+    for i in range(0, len(modlist), 1):
         # print(i, modlist[i], temp[modlist[i]])
-        result[temp[num]] = num
-        temp[num] -= 1
+        result[temp[modlist[i]]] = modlist[i]
+        div_res[temp[modlist[i]]] = divlist[i]
+        temp[modlist[i]] -= 1
+    result.pop(0)
+    div_res.pop(0)
+    # print("mod= ", modlist)
+    # print("div= ", div_res)
     print("sorted ", result)
+    sortedlist = [(10*div_res[i] + result[i]) for i in range(len(result))]
+    print("! ", sortedlist)
+    # return sortedlist
     # print("count ", temp)
-counting_sort(modlist)
+
+counting_sort(divlist, modlist)
 
 #print(list_sorted)
 
